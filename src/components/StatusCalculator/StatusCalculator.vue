bcecae66b513a475441c9bfc1a8db2172e29d0f8
felipeferrari40 <felipeferrari40@gmail.com>
2023-05-04 11:06:02 -0300

    selected arrumado

--- src/components/StatusCalculator/StatusCalculator.vue
+++ src/components/StatusCalculator/StatusCalculator.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="flex">
     <div class="flex">
-      <form>
-        <div><input type="number" placeholder="STR" v-model="data.STR" min="1" max="130"></div>
-        <div><input type="number" placeholder="AGI" v-model="data.AGI" min="1" max="130"></div>
-        <div><input type="number" placeholder="VIT" v-model="data.VIT" min="1" max="130"></div>
-        <div><input type="number" placeholder="INT" v-model="data.INT" min="1" max="130"></div>
-        <div><input type="number" placeholder="DEX" v-model="data.DEX" min="1" max="130"></div>
-        <div><input type="number" placeholder="LUK" v-model="data.LUK" min="1" max="130"></div>
+      <form class="w-32">
+        <div><input type="number" placeholder="STR" v-model="data.STR" min="1" max="130"><span></span><span
+            v-if="data.bless || data.sticks || data.foods">+ {{ data.blessBonus + data.sticksBonus + data.foodsBonus
+            }}</span></div>
+        <div><input type="number" placeholder="AGI" v-model="data.AGI" min="1" max="130"><span>{{ }}</span><span
+            v-if="data.sticks || data.foods">+ {{ data.sticksBonus + data.foodsBonus }}</span></div>
+        <div><input type="number" placeholder="VIT" v-model="data.VIT" min="1" max="130"><span>{{ }}</span><span
+            v-if="data.sticks || data.foods">+ {{ data.sticksBonus + data.foodsBonus }}</span></div>
+        <div><input type="number" placeholder="INT" v-model="data.INT" min="1" max="130"><span>{{ }}</span><span
+            v-if="data.bless || data.sticks || data.foods">+ {{ data.blessBonus + data.sticksBonus + data.foodsBonus
+            }}</span></div>
+        <div><input type="number" placeholder="DEX" v-model="data.DEX" min="1" max="130"><span>{{ }}</span><span
+            v-if="data.bless || data.sticks || data.foods">+ {{ data.blessBonus + data.sticksBonus + data.foodsBonus
+            }}</span></div>
+        <div><input type="number" placeholder="LUK" v-model="data.LUK" min="1" max="130"><span>{{ }}</span><span
+            v-if="data.sticks || data.foods">+ {{ data.sticksBonus + data.foodsBonus }}</span></div>
       </form>
     </div>
     <form>
@@ -17,10 +26,10 @@
       </div>
       <div><label>Bless</label><input class="mx-3" id="bless" type="checkbox" @change="blessUp()" v-model="data.bless">
       </div>
-      <div><label>Classe :</label></div>
       <div>
-        <select class="w-42" v-model="selected">
-          <option v-for="charClass in charClasses" :key="charClass.name">{{ charClass.name }}</option>
+        <p>Classe : {{ selected }} </p><br/>
+        <select class="w-42" v-model="selected"> 
+          <option v-for="charClass in charClasses" :key="charClass.id" :value='charClass.name'>{{ charClass.name }}</option>
         </select>
       </div>
     </form>
@@ -35,24 +44,27 @@
 import { reactive, computed, ref } from 'vue';
 
 const charClasses = ref([
-  {name: 'Rune Knight'},
-  {name: 'Guillotin Cross'},
-  {name: 'Arch Bishop'},
-  {name: 'Ranger'},
-  {name: 'Warlock'},
-  {name: 'Mechanic'},
-  {name: 'Royal Guard'},
-  {name: 'Shadow Chaser'},
-  {name: 'Shura'},
-  {name: 'Maestro'},
-  {name: 'Wanderer'},
-  {name: 'Sorcerer'},
-  {name: 'Geneticist'},
-  {name: 'Rebelion'},
-  {name: 'Kagerou/Oboro'},
-  {name: 'Soul Reaper'},
-  {name: 'Star Emperor'}
+  { name: 'Rune Knight', id: 1},
+  { name: 'Guillotine Cross', id: 2 },
+  { name: 'Arch Bishop', id: 3},
+  { name: 'Ranger', id:4 },
+  { name: 'Warlock', id:5 },
+  { name: 'Mechanic', id:6 },
+  { name: 'Royal Guard', id:7 },
+  { name: 'Shadow Chaser', id:8 },
+  { name: 'Shura', id:9 },
+  { name: 'Maestro', id:10 },
+  { name: 'Wanderer', id:11 },
+  { name: 'Sorcerer', id:12 },
+  { name: 'Geneticist', id:13 },
+  { name: 'Rebelion', id:14 },
+  { name: 'Kagerou/Oboro', id:15 },
+  { name: 'Soul Reaper', id:16 },
+  { name: 'Star Emperor', id:17 }
 ])
+
+const selected = ref('')
+
 
 const data = reactive({
   STR: null,
@@ -97,16 +109,14 @@ function blessUp() {
 //let blessUp = 10
 
 const varibleCast = computed(
-  () => Math.round(2 *(data.DEX + data.sticksBonus + data.foodsBonus + data.blessBonus) + (data.INT + data.sticksBonus + data.foodsBonus + data.blessBonus))
+  () => Math.round(((2 * ((data.DEX || 1) + data.sticksBonus + data.foodsBonus + data.blessBonus) + ((data.INT || 1) + data.sticksBonus + data.foodsBonus + data.blessBonus))))
 )
 
 </script>
 
 
-<style>
-form div {
+<style>form div {
   margin: 5px;
   padding: 5px;
   text-align: start;
-}
-</style>
+}</style>
